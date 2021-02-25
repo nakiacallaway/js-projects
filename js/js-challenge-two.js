67ccad59eec2c7308360362db48b2362b1ae166c
Nakia Callaway <nik.callaway@outlook.com>
2021-02-24 16:55:42 -0800

    day 3 exercises 3-5

--- js/js-challenge-two.js
+++ js/js-challenge-two.js
@@ -1,3 +1,5 @@
+//Challenges begin on line 90.  All the rest is my notes.
+
 /*let value1 = 35;
 let value2 = 45;
 
@@ -26,9 +28,7 @@ if (a > b) {
 }
 */
 //For loop counter
-for (let i = 1; i <= 100; i += 2) {
-    console.log(i);
- }
+
 
 
 //If/then
@@ -90,6 +90,13 @@ function showMessage() {
   
   showMessage(); // You have to call the function for it to work
 */
+
+//Challenge 1
+for (let i = 1; i <= 100; i += 2) {
+  console.log(i);
+}
+
+//Challenge 2
 const welmsg = () => {
 let firstName = 'Nakia';
 let lastName = 'Callaway';
@@ -100,6 +107,38 @@ const fullName = (arg1, arg2) => {
     alert('Welcome '+ arg1 + ' ' + arg2);
 }
   
-  welmsg()
+welmsg()
 
-  
+//Challenge 3  
+const howOld = ()=> {
+  let userAge = prompt("Hi, how old are you?");
+  //if the prompt is over 13
+  if (userAge >= 13 ) alert("You are okay to enter.") 
+  //if the prompt is under 13
+  else alert("Come back when you're older.")
+}
+//debugger
+howOld();
+
+//Challenge 4
+const temp = () => {
+  let celsius = 24.2;
+  console.log('it is', celsius * 1.8 + 32, 'degrees outside. Do you think that is too hot?');
+  }
+//debugger  
+temp();
+
+//Challenge 5
+const guessNum = ()=> {
+  let numGuess = prompt("Pick a number!");
+  let numAnswer = '7';
+if (numGuess != numAnswer) {
+  alert('Wrong guess this time, try again!');
+  guessNum();
+  return;
+}
+
+else {
+  alert('You guessed it!');
+}
+}
